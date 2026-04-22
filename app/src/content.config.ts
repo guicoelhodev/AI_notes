a20a96dfeb4b697f10f84c3d103635875331577d
guicoelhodev <gs.coelho_dev@outlook.com>
2026-04-22 15:13:39 -0300

    refactor: remove App project from Astro

--- app/src/content.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection } from "astro:content";
-import { glob } from "astro/loaders";
-
-const docs = defineCollection({
-	loader: glob({ pattern: "**/*.md", base: "../docs" }),
-});
-
-export const collections = { docs };
